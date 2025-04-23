79dba841e60534df68d9a5d122fcd059da17c15f
abt0708 <203622393+abt0708@users.noreply.github.com>
2025-04-23 16:07:55 +0000

    [finish?]7ここすごく不安Unpermitted parameter: :password_confirmation謎のエラーが勝手になくなってた。

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  # ユーザ名が空でなく、かつ2文字以上の名前を要求するよう設定。
+  validates :name, presence: true
+end

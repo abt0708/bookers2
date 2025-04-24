81a92f93d9a2907d1512a8a410ea4ba08379ca1d
abt0708 <203622393+abt0708@users.noreply.github.com>
2025-04-24 08:29:56 +0000

    [finish]13userコントローラのさくせい

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :books, only: [:new, :index, :show, :edit]
+  resource :users, only: [:show, :edit]
   devise_for :users
   root to: "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

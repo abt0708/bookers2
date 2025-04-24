8c00662e41186b709684d27412707d70fbdb741e
abt0708 <203622393+abt0708@users.noreply.github.com>
2025-04-24 08:23:34 +0000

    [finish]11

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
   
   # ユーザ名が空でなく、かつ2文字以上の名前を要求するよう設定。
   validates :name, presence: true
+  has_many :books, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :book, only: [:new, :index, :show]
+  resources :books, only: [:new, :index, :show, :edit]
   devise_for :users
   root to: "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

cd0ee96f381630821ec534aa8c498e951a21228a
abt0708 <203622393+abt0708@users.noreply.github.com>
2025-05-05 11:48:33 +0000

    [finish}38

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,8 @@ class UsersController < ApplicationController
     @book = Book.new
   end
   def show
-    @user = User.find(params[:id])
+    @user = User.find(params[:id])  # そのユーザー情報を取得
+    @books = @user.books  # ユーザーが投稿した本を取得
   end
 
   def edit

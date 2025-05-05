0e5bf14d72f92502af1ded02d41f9311895cd294
abt0708 <203622393+abt0708@users.noreply.github.com>
2025-05-05 10:28:34 +0000

    [finish]37

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all  # すべてのユーザーを取得
+    @users = User.all
+    @book = Book.new
   end
   def show
     @user = User.find(params[:id])

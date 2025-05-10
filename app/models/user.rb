ba927147a8740f03f5c9f17706f7b198248dedf8
abt0708 <203622393+abt0708@users.noreply.github.com>
2025-05-10 08:20:00 +0000

    [finish]49

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  # ユーザ名が空でなく、かつ2文字以上の名前を要求するよう設定。
-  validates :name, presence: true
+
   has_many :books, dependent: :destroy
+  validates :name, presence: true, uniqueness: true, length: { in: 2..20 }
+  validates :introduction, length: { maximum: 50 }
 end
